63a6ea97016621074f983a6fec8b56c652dae10d
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-07-26 14:12:03 +0000

    details for index file

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 import connectDb from './db/db.js'
 import dotenv from 'dotenv'
+// setting up the dotenv configuration
 dotenv.config({
     path:'./env'
 })
 
-
+// calling function to connect the database
 connectDb()

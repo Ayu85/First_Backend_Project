adf577d50d94bc63df35c71cd0de7403751a9b66
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-07-26 11:06:37 +0000

    connection done

--- db/db.js
+++ db/db.js
@@ -1,10 +1,10 @@
-const mongoose = require('mongoose')
-import { DB_NAME } from '../constants'
+import mongoose from 'mongoose';
+import { DB_NAME } from '../constants.js'
 
 const connectDb = async () => {
     try {
         const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
-        console.log("Mongodb connected:- ", connectionInstanceF);
+        console.log("Boom !!!! Mongodb connected:- ", connectionInstance.connection.host);
     } catch (error) {
         console.log("Mongodb Connection Error:- ", error);
     }

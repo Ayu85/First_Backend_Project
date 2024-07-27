97f6ccb5ad4049cf4abcaea5dec80125ee0aec07
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-07-27 09:18:26 +0000

    setting up cors and parser

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 import express from 'express'
-const app=express()
+import cors from 'cors'
+import cookieParser from 'cookie-parser'
+const app = express()
+// to enable cross origin sharing
+app.use(cors({
+    origin: '*'
+}))
 // function to start the express server
-export const startServer=()=>{
-    app.listen(process.env.PORT,()=>{
-        console.log("App started on port ",process.env.PORT);
+export const startServer = () => {
+    app.listen(process.env.PORT, () => {
+        console.log("App started on port ", process.env.PORT);
     })
 }
 export default app;
